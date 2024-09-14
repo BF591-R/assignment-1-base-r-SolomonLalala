abc48c1817135f24454d3127ea196e6001385053
shihong Yuan <yuansh24@scc-wl2.scc.bu.edu>
2024-09-14 18:13:55 -0400

    chunck before summarize matrix were completed

--- main.R
+++ main.R
@@ -44,7 +44,7 @@ less_than_zero <- function(x) {
 #' [2,]  TRUE FALSE FALSE
 #' [3,] FALSE FALSE FALSE
 is_between <- function(x, a, b) {
-    return(NULL)
+    return(a < x & x < b)
 }
 
 #' Return the values of the input vector that are not NA
@@ -61,7 +61,7 @@ is_between <- function(x, a, b) {
 #' rm_na(x)
 #' [1] 1 2 3
 rm_na <- function(x) {
-    return(NULL)
+    return(x[!is.na(x)])
 }
 
 #' Calculate the median of each row of a matrix
@@ -80,7 +80,7 @@ rm_na <- function(x) {
 #' [1] 1 4 7
 #' 
 row_medians <- function(x) {
-    return(NULL)
+    apply(x, 1, median)
 }
 
 #' Evaluate each row of a matrix with a provided function
@@ -105,7 +105,7 @@ row_medians <- function(x) {
 #' summarize_rows(m, mean)
 #' [1] 2 5 8
 summarize_rows <- function(x, fn, na.rm=FALSE) {
-    return(NULL)
+    apply(x, 1, fn)
 }
 
 #' Summarize matrix rows into data frame
